321b4b4b628d4dbfab12558fdd25fdeb8acb755e
shanur-marsplay <shanur@marsplay.co>
2019-12-26 00:45:24 +0530

    Adding a readme file, wrote basic routes for fetching movie by title, id and another one for getting all the locations the movie was shot at

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Find your travel destinations, from famous movie spot
 > More on it here
 > https://node-postgres.com/api/pool
 
+
+
+Furthur improvements
+~ Add a global exception handler to catch uncaught exceptions
+~ Add a slack integration  for that exception
+~ Move service code to another file
+~ To add a pagination service to all get requests that return a list
+~ Create a different service file, currently it doesnot make any sense to add it since all queries are single line
--- routes/movies.js
+++ routes/movies.js
@@ -3,12 +3,45 @@ var router = express.Router();
 const db = require('../db')
 
 
-/* GET movie by id. */
+/**
+ * Movie Suggestor, will be used for searching. Applied GIN indexing for full text search on the db
+ * Get movie by @query {title, production_company}
+ * @Todo Add another parameter called production comapny to the query
+ * GET /movies?title=sha
+ */
+router.get('/', async function(req, res, next) {
+    const { title } = req.query;
+    const {rows} = await db.query('select * from movie where lower(title) like $1', [`%${title}%`]);
+    res.send(rows);
+})
+
+
+
+/**
+ * Sends back all locations where a particular movie was shot, once we have populated movies we
+ * will use movie ids, or title to fetch locations where the movie was shot
+ * Get movie by @query {title} title has to be a URL Encoded string 
+ * @Todo Add another parameter called production comapny to the query
+ * GET /movies/shotLocation?title=A%20Night%20Full%20of%20Rain
+ */
+router.get('/shotLocation', async function(req, res, next) {
+    let { title } = req.query;
+    title = decodeURI(title);
+    const {rows} = await db.query('select id, locations from movie where title=$1', [title]);
+    res.send(rows);
+})
+
+
+/**
+ * GET /movies/:id
+ * get movies by id
+ */
 router.get('/:id', async function(req, res, next) {
-    const { id } = req.params
-    // const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id])
-    const {rows} = await db.query('select * from movie limit 1');
-    res.send(rows[0]);
-});
+    let { id } = req.params;
+    const {rows} = await db.query('select id, locations from movie where id=$1', [id]);
+    res.send(rows);
+})
+
+
 
 module.exports = router;
