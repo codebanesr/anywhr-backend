fb296fad728ce2816b08757b8b72f8081b442538
shanur-marsplay <shanur@marsplay.co>
2019-12-26 01:08:26 +0530

    Added pagination to the required routes

--- README.md
+++ README.md
@@ -16,3 +16,8 @@ Furthur improvements
 ~ Move service code to another file
 ~ To add a pagination service to all get requests that return a list
 ~ Create a different service file, currently it doesnot make any sense to add it since all queries are single line
+
+
+~ `Add a movie table`
+~ ` Add another route called user, where the user can save his favourite movie shot locations, and can revisit it. Kind of like a cart to store shopping items. ` 
+~ ` Allow him to rate these locations from 1 to 5 `
--- routes/movies.js
+++ routes/movies.js
@@ -2,16 +2,30 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db')
 
+/** 
+ * GET /movies
+ * get all movies
+ * @query {page, perPage} for pagination, setting a default of 20 and page 1
+ * GET /movies?title=sha&page=1&perPage=1
+ */
+router.get('/', async function(req, res, next) {
+    let { page=1, perPage=20 } = req.query;
+    const offset = (page-1)*perPage;
+    const {rows} = await db.query('select id, title, locations from movie order by title asc limit $1 offset $2', [perPage, offset]);
+    res.send(rows);
+})
+
 
 /**
  * Movie Suggestor, will be used for searching. Applied GIN indexing for full text search on the db
- * Get movie by @query {title, production_company}
+ * Get movie by @query {title}
  * @Todo Add another parameter called production comapny to the query
- * GET /movies?title=sha
+ * GET /movies/suggest?title=sha&page=1&perPage=1
  */
-router.get('/', async function(req, res, next) {
-    const { title } = req.query;
-    const {rows} = await db.query('select * from movie where lower(title) like $1', [`%${title}%`]);
+router.get('/suggest', async function(req, res, next) {
+    let { title, page=1, perPage=20 } = req.query;
+    const offset = (page-1)*perPage;
+    const {rows} = await db.query('select * from movie where lower(title) like $1 limit $2 offset $3', [`%${title}%`, perPage, offset]);
     res.send(rows);
 })
 
@@ -41,7 +55,6 @@ router.get('/:id', async function(req, res, next) {
     const {rows} = await db.query('select id, locations from movie where id=$1', [id]);
     res.send(rows);
 })
-
 
 
 module.exports = router;
