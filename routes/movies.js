233f2ed8db2fe84226e27017612b703892186bce
shanur-marsplay <shanur@marsplay.co>
2019-12-29 01:27:48 +0530

    added a seperate service file, will be adding ajv schema validation next

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var router = express.Router();
-const db = require('../db')
+const express = require('express');
+const router = express.Router();
+const {
+    getAllMovies, getSuggestedMovies,getMovieShootingLocation,getMovieById
+} = require('../service/moviesService');
 
 /** 
  * GET /movies
@@ -11,9 +13,8 @@ const db = require('../db')
 router.get('/', async function(req, res, next) {
     let { page=1, perPage=20 } = req.query;
     const offset = (page-1)*perPage;
-    const {rows} = await db.query('select * from movie order by title asc limit $1 offset $2', [perPage, offset]);
-    const {rows: [{total}]} = await db.query('select count(*) as total from movie');
-    res.send({data: rows, total});
+    const result = await getAllMovies(perPage, offset);
+    res.send(result);
 })
 
 
@@ -27,9 +28,8 @@ router.get('/suggest', async function(req, res, next) {
     let { title, page=1, perPage=20 } = req.query;
     title=title.toLocaleLowerCase();
     const offset = (page-1)*perPage;
-    const {rows} = await db.query('select * from movie where lower(title) like $1 limit $2 offset $3', [`%${title}%`, perPage, offset]);
-    const {rows: [{total}]} = await db.query('select count(*) as total from movie where lower(title) like $1', [`%${title}%`]);
-    res.send({data: rows, total});
+    const result = await getSuggestedMovies(title, perPage, offset)
+    res.send(result);
 })
 
 /**
@@ -41,11 +41,8 @@ router.get('/suggest', async function(req, res, next) {
  */
 router.get('/shotLocation', async function(req, res, next) {
     let { title } = req.query;
-    title = decodeURI(title);
-    const {rows} = await db.query(`select *,
-        (select json_agg(locations) as locations from movie_location where title=$1)
-        from movie where title=$1`, [title]);
-    res.send(rows[0]);
+    const result = await getMovieShootingLocation(title);
+    res.send(result);
 })
 
 
@@ -55,7 +52,7 @@ router.get('/shotLocation', async function(req, res, next) {
  */
 router.get('/:id', async function(req, res, next) {
     let { id } = req.params;
-    const {rows} = await db.query('select title from movie where id=$1', [id]);
+    const result = await getMovieById(id);
     res.send(rows);
 })
 
---
+++ service/moviesService.js
@@ -0,0 +1,36 @@
+const db = require('../db')
+
+const getAllMovies = async(perPage, offset) => {
+    const {rows} = await db.query('select * from movie order by title asc limit $1 offset $2', [perPage, offset]);
+    const {rows: [{total}]} = await db.query('select count(*) as total from movie');
+    return {data: rows, total}
+}
+
+
+const getSuggestedMovies = async(title, perPage, offset) => {
+    const {rows} = await db.query('select * from movie where lower(title) like $1 limit $2 offset $3', [`%${title}%`, perPage, offset]);
+    const {rows: [{total}]} = await db.query('select count(*) as total from movie where lower(title) like $1', [`%${title}%`]);
+    return {data: rows, total}
+}
+
+const getMovieShootingLocation = async(title) => {
+    title = decodeURI(title);
+    const {rows} = await db.query(`select *,
+        (select json_agg(locations) as locations from movie_location where title=$1)
+        from movie where title=$1`, [title]);
+    return rows[0];
+}
+
+const getMovieById = async(id) => {
+    const {rows} = await db.query('select title from movie where id=$1', [id]);
+    return rows;
+}
+
+
+
+module.exports = {
+    getAllMovies,
+    getSuggestedMovies,
+    getMovieShootingLocation,
+    getMovieById
+}
