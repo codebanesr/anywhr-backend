13efb2c47a6cda36be453ef68f189eeeb91b699a
shanur-marsplay <shanur@marsplay.co>
2019-12-29 13:39:56 +0530

    added documentation in static files

--- app.js
+++ app.js
@@ -13,11 +13,16 @@ require('./starter/initDecorators')(app);
 
 // static file initializers
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'documentation')));
 
 // Route initializing middleware functions
 require('./starter/initRoutes')(app);
 
 // static file handling
+app.use('/documentation', (req, res, next)=>{
+  res.sendFile(path.join(__dirname, 'documentation/index.html'));
+})
+
 app.use('*', (req, res, next)=>{
   res.sendFile(path.join(__dirname, 'public/index.html'));
 })
